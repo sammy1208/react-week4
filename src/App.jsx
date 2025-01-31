b23e33415d6b11f78af321377bd06c735f5049a5
sammy1208 <34804688+sammy1208@users.noreply.github.com>
2025-01-31 16:02:13 +0800

    GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,22 @@
-import { useEffect, useRef, useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
-import axios from 'axios';
-import { Modal } from 'bootstrap';
-import LoginPage from "./pages/LoginPage"
-import ProductPage from './pages/ProductPage';
+import LoginPage from "./pages/LoginPage";
+import ProductPage from "./pages/ProductPage";
 // import ProductPage from './pages/ProductPage';
 // import './App.css'
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-const API_PATH = import.meta.env.VITE_API_PATH;
-
-const defaultModalState = {
-  imageUrl: "",
-  title: "",
-  category: "",
-  unit: "",
-  origin_price: "",
-  price: "",
-  description: "",
-  content: "",
-  is_enabled: 0,
-  imagesUrl: [""]
-};
-
 function App() {
-
   const [isAuth, setIsAuth] = useState(false);
-
-
-
- 
-
-
 
   return (
     <>
       {isAuth ? (
-        <ProductPage setIsAuth={setIsAuth}/>
+        <ProductPage setIsAuth={setIsAuth} />
       ) : (
-        <LoginPage setIsAuth={setIsAuth}/>
+        <LoginPage setIsAuth={setIsAuth} />
       )}
-  
-
     </>
   );
-  
 }
 
-export default App
+export default App;
